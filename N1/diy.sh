28f5cd0af668953d39df6daed46d12090b0b72b6
Rangtian <Rangtian@users.noreply.github.com>
2025-05-15 20:55:39 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -11,9 +11,9 @@ git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 rm -rf feeds/luci/applications/luci-app-passwall
 cp -rf clone/passwall/luci-app-passwall feeds/luci/applications/
 
-# git clone https://github.com/vernesong/OpenClash --depth=1 clone/openclash
-# rm -rf feeds/luci/applications/luci-app-openclash
-# cp -rf clone/openclash/luci-app-openclash feeds/luci/applications/
+git clone https://github.com/vernesong/OpenClash --depth=1 clone/openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+cp -rf clone/openclash/luci-app-openclash feeds/luci/applications/
 
 # Clean packages
 rm -rf clone

cdf0e224fde73913a747bb67af52f751c054b745
Rangtian <Rangtian@users.noreply.github.com>
2025-08-02 01:54:33 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -4,9 +4,9 @@
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
 
-git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
-rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/passwall/luci-app-passwall feeds/luci/applications/
+# git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+# rm -rf feeds/luci/applications/luci-app-passwall
+# cp -rf clone/passwall/luci-app-passwall feeds/luci/applications/
 
 git clone https://github.com/vernesong/OpenClash --depth=1 clone/openclash
 rm -rf feeds/luci/applications/luci-app-openclash
